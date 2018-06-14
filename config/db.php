ae25f44c0d7057d97c3aebb28fd8336320d860cd
srinivasanmf <v-skrishnamoorthy@GABRIELNY.COM>
2018-06-14 17:19:55 -0400

    db change

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=test.dev.z9round.com;dbname=mango_test',
+    'dsn' => 'mysql:host=45.33.85.253;dbname=mango_test',
     'username' => 'mango_test',
     'password' => 'wWyS+@tXdw9ZBD=',
     'charset' => 'utf8',
